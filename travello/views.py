66ee5f17193563f4780830ff42dc264429375a81
Kuldeep <2016ucs0020@iitjammu.ac.in>
2020-06-01 11:08:15 +0530

    destination data is coming from db now

--- travello/views.py
+++ travello/views.py
@@ -5,27 +5,28 @@ from .models import Destination
 # Create your views here.
 def index(request):
 
-    dest1 = Destination()
-    dest1.name = 'Mumbai'
-    dest1.desc = "Mumbai in India."
-    dest1.img = 'destination_1.jpg'
-    dest1.price = 700
-    dest1.offer = False
+    # dest1 = Destination()
+    # dest1.name = 'Mumbai'
+    # dest1.desc = "Mumbai in India."
+    # dest1.img = 'destination_1.jpg'
+    # dest1.price = 700
+    # dest1.offer = False
+    #
+    # dest2 = Destination()
+    # dest2.name = 'Delhi'
+    # dest2.desc = 'Polluted City.'
+    # dest2.img = 'destination_2.jpg'
+    # dest2.price = 700
+    # dest2.offer = True
+    #
+    # dest3 = Destination()
+    # dest3.name = 'Kerala'
+    # dest3.desc = 'Green City.'
+    # dest3.img = 'destination_3.jpg'
+    # dest3.price = 700
+    # dest3.offer = False
+    #
+    # dests = [dest1, dest2, dest3]
 
-    dest2 = Destination()
-    dest2.name = 'Delhi'
-    dest2.desc = 'Polluted City.'
-    dest2.img = 'destination_2.jpg'
-    dest2.price = 700
-    dest2.offer = True
-
-    dest3 = Destination()
-    dest3.name = 'Kerala'
-    dest3.desc = 'Green City.'
-    dest3.img = 'destination_3.jpg'
-    dest3.price = 700
-    dest3.offer = False
-
-    dests = [dest1, dest2, dest3]
-
+    dests = Destination.objects.all()
     return render(request, 'index.html', {'dests': dests})
