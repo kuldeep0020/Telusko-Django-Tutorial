b98f6799373e9ef1ce59d65bddd9c76f7d9b9c3b
Kuldeep <2016ucs0020@iitjammu.ac.in>
2020-06-02 09:58:07 +0530

    Made login, register and logout functionality

--- Telusko/urls.py
+++ Telusko/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('travello.urls')),
     path('calc/', include('calc.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
